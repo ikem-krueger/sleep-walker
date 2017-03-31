92a9b4e514c5f93d9e13540e4e5fa063ec86fd60
Ikem Krueger <ikem.krueger@googlemail.com>
2017-03-31 14:28:23 +0200

    Pretty print output

--- sleepwalker2.py
+++ sleepwalker2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import os
 import subprocess
 import ewmh
 from time import sleep
@@ -22,10 +21,15 @@ def get_visible_window_ids():
 def wakeup_windows(window_list=None):
     print("Waking windows up...")
 
+    pids = set()
+
     for wid in ewmh.getClientList():
         pid = int(ewmh.getWmPid(wid))
 
-        print("Wake window up...")
+        pids.add(pid)
+
+    for pid in pids:
+        print("Wake up process with pid %s..." % pid)
 
         subprocess.call(["kill", "-SIGCONT", str(pid)])
 
@@ -35,17 +39,20 @@ def sleep_windows(window_list=None):
     active_window = ewmh.getActiveWindow()
     active_window_pid = ewmh.getWmPid(active_window)
 
+    pids = set()
+
     for wid in ewmh.getClientList():
         pid = int(ewmh.getWmPid(wid))
 
         if pid == active_window_pid:
-            print("Skip active window...")
-
             continue
         else:
-            print("Put window to sleep...")
+            pids.add(pid)
 
-            subprocess.call(["kill", "-SIGSTOP", str(pid)])
+    for pid in pids:
+        print("Put process to sleep with pid %s..." % pid)
+
+        subprocess.call(["kill", "-SIGSTOP", str(pid)])
 
 if __name__ == "__main__":
     timeout = 2.0
@@ -56,14 +63,14 @@ if __name__ == "__main__":
     while True:
         idle = get_idle_time()
 
-        print("Idle for %s seconds..." % idle)
-
         if idle >= timeout: # if n-seconds idle...
             if sleeping == False:
                 sleep_windows()
 
                 sleeping = True
         else:
+            print("Idle for %s seconds..." % idle)
+
             if sleeping == True:
                 wakeup_windows()
 
